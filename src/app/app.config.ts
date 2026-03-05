c88203544dcd1bfb5f25a1650b97018934e8ee1c
fabien zibi <fabien.zibi@gmail.com>
2026-03-06 00:13:58 +0100

    add db

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'AIzaSyAtbXA_ryL1IAQ0B_N0IYGBtFEevh4Bf-M',
+        authDomain: 'bcbc-3716f.firebaseapp.com',
+        projectId: 'bcbc-3716f',
+        storageBucket: 'bcbc-3716f.firebasestorage.app',
+        messagingSenderId: '2601790820',
+        appId: '1:2601790820:web:a6116b4935d4b181505e71',
+        measurementId: 'G-3RRGCBP2KE',
+      }),
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
--- src/app/pages/schedule/schedule.ts
+++ src/app/pages/schedule/schedule.ts
@@ -2,15 +2,32 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GamesService } from '../../services/games.service';
 import { Game, GameLocation } from '../../models/game.model';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface GameData {
+  opponent: string;
+  date: string;
+}
 
 @Component({
   selector: 'app-schedule',
   imports: [CommonModule],
   templateUrl: './schedule.html',
   styleUrl: './schedule.scss',
-  standalone: true
+  standalone: true,
 })
 export class Schedule implements OnInit {
+  firestore = inject(Firestore);
+
+  public readonly $games: Observable<GameData[]>;
+
+  constructor() {
+    const itemCollection = collection(this.firestore, 'schedule');
+    this.$games = collectionData(itemCollection) as Observable<GameData[]>;
+  }
+
+  //
   allGames: Game[] = [];
 
   GameLocation = GameLocation;
